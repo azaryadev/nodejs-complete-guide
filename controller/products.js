81422ea0fbe08f14271397309bdceb12d3987744
Aldy Azarya <aldy@autentika.id>
2025-05-21 16:20:57 +0700

    adding models

--- controller/products.js
+++ controller/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   console.log("add product path middleware");
@@ -10,19 +10,19 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  console.log(req.body);
-  products.push({ title: req.body.title });
-
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+    });
   });
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
 };
